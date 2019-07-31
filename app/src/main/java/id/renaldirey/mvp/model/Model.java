6a7e11ce1203f76b1a05fb10d0c8ca93ae6621f4
renaldi2108 <renaldi2108@gmail.com>
2019-07-31 18:56:24 +0700

    change a model

--- app/src/main/java/id/renaldirey/mvp/model/Model.java
+++ app/src/main/java/id/renaldirey/mvp/model/Model.java
@@ -16,39 +16,6 @@ public class Model {
     @SerializedName("body")
     String body;
 
-    private String nama, jeniskelamin;
-    private Integer umur;
-
-    public Model(String nama, String jeniskelamin, Integer umur){
-        this.nama = nama;
-        this.jeniskelamin = jeniskelamin;
-        this.umur = umur;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getJeniskelamin() {
-        return jeniskelamin;
-    }
-
-    public void setJeniskelamin(String jeniskelamin) {
-        this.jeniskelamin = jeniskelamin;
-    }
-
-    public Integer getUmur() {
-        return umur;
-    }
-
-    public void setUmur(Integer umur) {
-        this.umur = umur;
-    }
-
     public int getUserId() {
         return userId;
     }
